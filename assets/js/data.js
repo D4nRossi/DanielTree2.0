2fbc6aadb7eab1f85003faee83b505de0b2d33a6
Daniel Rossi de Amorim <danielrossi5678@gmail.com>
2023-03-19 13:14:15 -0300

    Colocando serviço de contador de visitantes

--- assets/js/data.js
+++ assets/js/data.js
@@ -58,3 +58,31 @@ export const layout1 = document.querySelector('#layout1')
 export const layout2 = document.querySelector('#layout2')
 export const layout3 = document.querySelector('#layout3')
 export const layout4 = document.querySelector('#layout4')
+
+export function setCookie(name, value, days) {
+    var expires = "";
+    if (days) {
+      var date = new Date();
+      date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+      expires = "; expires=" + date.toUTCString();
+    }
+    document.cookie = name + "=" + (value || "") + expires + "; path=/";
+  }
+
+  export function getCookie(name) {
+    var nameEQ = name + "=";
+    var ca = document.cookie.split(';');
+    for (var i = 0; i < ca.length; i++) {
+      var c = ca[i];
+      while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+      if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+    }
+    return null;
+  }
+  var contador = getCookie("contador");
+if (contador === null) {
+  contador = 0;
+}
+document.getElementById("contador").innerHTML = contador;
+contador++;
+setCookie("contador", contador, 365);
